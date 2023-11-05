86aaad9e79bacdc3c95a69e21e1f348d79271cec
Vladyslav Yanchenko <vyanch72@gmail.com>
2023-11-05 22:04:47 +0100

    account fix

--- src/main/resources/templates/data.sql
+++
@@ -1,8 +0,0 @@
-# insert into manager (first_name, last_Name, create_at)
-# values ("Tom", "Smith", '2023-01-01');
-#
-# # insert into product (name, status, currencyCode, interest_rate, total, createAt)
-# insert into product (interest_rate, total, create_at)
-# values (10.0, 10000, '2023-01-01');
-# # values ('ProductName.CARD', 'ProductStatus.ACTIVE', 'CurrencyCode.USD', 10.0, 10000, '2023-01-01');
-
